9554188be5b8e5da7c93d436df9c3d7b04a18000
Andre Kraemer <andre@andrekraemer.de>
2016-02-01 10:20:32 +0100

    forgot to save ...

--- Demo2/Scripts/scripts_solution.js
+++ Demo2/Scripts/scripts_solution.js
@@ -1,6 +1,6 @@
 (function(){
     "use strict";
-        // don't do that
+        // avoid doing that
         //document.write('<p>Ich komme aus JavaScript</p>');
         var elem = document.getElementsByTagName("p")[0];
         var color = elem.style.color;
